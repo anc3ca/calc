0190e1a842be023b6db48b533d943d82ab3928f1
Esther <81744133+tomiiko@users.noreply.github.com>
2023-03-28 08:47:46 +0300

    pievienoju funkcijas :)))

---
+++ Jauna mape/script.js
@@ -0,0 +1,29 @@
+const plusins = document.querySelector('.js-plus');
+const inputins = document.querySelector('.js-input');
+
+console.log(plusins)
+console.log(inputins)
+
+const Add = (cipars1, cipars2) => {
+
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) =>{
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    console.log("A")
+})
+
+console.log(Add(2,3))
+console.log(Multiply(6,8))
