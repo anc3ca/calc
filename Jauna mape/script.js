0779836fb4c001b0664866d18783c6f4bf6c86bb
Esther <81744133+tomiiko@users.noreply.github.com>
2023-04-18 08:43:09 +0300

    powpow

--- Jauna mape/script.js
+++ Jauna mape/script.js
@@ -4,20 +4,13 @@ const minus = document.querySelector('.js-minus');
 const divide = document.querySelector('.js-divide');
 const inputins1 = document.querySelector('.js-input1');
 const inputins2 = document.querySelector('.js-input2');
-const answer = doccument.querySelector('js-result');
-
-let firstValue = inputins1.value
-let secondValue = inputins2.value
-
-console.log(plusins)
-console.log(inputins)
+const answer = document.querySelector('.js-result');
 
 const Add = (cipars1, cipars2) => {
-
     return cipars1 + cipars2
 }
 
-const Subtract = (cipars1, cipars2) =>{
+const Subtract = (cipars1, cipars2) => {
     return cipars1 - cipars2
 }
 
@@ -29,15 +22,35 @@ const Divide = (cipars1, cipars2) => {
     return cipars1 / cipars2
 }
 
-plusins.addEventListener('click', () => {
-    
-    answer.textContent = (Add
-        (parseInt(imputins1.value),parseInt(imputins2.value)))
+const clear = () => {
+    inputins1.value = ""
+    inputins2.value = ""
+}
 
+plusins.addEventListener('click', () => {
+    answer.textContent = Add(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
 })
 
-imputins1.value = ""
-imputins2.value = ""
+minus.addEventListener('click', () => {
+    answer.textContent = Subtract(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
+multiply.addEventListener('click', () => {
+    answer.textContent = Multiply(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
+divide.addEventListener('click', () => {
+    answer.textContent = Divide(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
 
 console.log("RAAAAHHHHHHHHH😋")
 console.log(Add(firstValue,secondValue))
